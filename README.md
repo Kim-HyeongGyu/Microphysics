297f3b807dd62d6d6f0024b8d41289707c09ff20
Kim-HyeongGyu <hyeonggyu.kim01@gmail.com>
2020-03-27 17:40:20 +0900

    Add namelist

--- README.md
+++ README.md
@@ -3,20 +3,21 @@ Repository for Microphysics II class in KNU.
 
 ## Structure
 ```
+/microphysics
 ├-- doc    # documentation for box model.
 └-- src    # source code
     ├- main
-    |  ├- driver.f90        # code for box model.
-    |  └- microphysics.f90  # Main code (e.g. module, ...)
+    |  ├- driver.f90            # code for box model.
+    |  └- microphysics.f90      # Main code (e.g. module, ...)
     └- test
-       ├- driver.f90       
-       └- microphysics.f90 
+       ├- driver.f90
+       ├- microphysics.f90
+       ├- constant.f90          # constant variables
+       ├- vert_advection.f90    # advection (in FMS)
+       ├- vert_advection.html
+       └- input.nml             # namelist
 ```
 
-## Tool
-- Programming language : `Fortran90`
-- Code notation : ???
-
-## Schedule
-- [ ] Meeting (2020.03.26 15:00~16:00)
+## Next meeting
+- 2020.03.31 14:00~
 
--- src/test/driver.f90
+++ src/test/driver.f90
@@ -1,6 +1,42 @@
 program driver
-    ! Program driver.f90
-    ! call procedure, ...
-    implicit none
-        
+use constant_mod, only: show_constant
+! use         advection_mod, only: compute_advection
+! use   vert_coordinate_mod, only: compute_vert_coord
+implicit none
+
+    ! Set variables
+    integer :: nz
+    real    :: ztop
+    real    :: GRAVITY, PI
+    ! real, dimension(n+1) :: array_foreward, array_backward
+    character(len=20) :: grid_dz
+    namelist /setup_list/ nz, ztop, grid_dz
+
+    open  (unit = 8, file = 'input.nml', delim = 'apostrophe')
+    read  (unit = 8, nml  = setup_list) 
+    close (unit = 8)
+
+    ! initialization
+    ! open  (91,file='./INPUT/T.dat',access='direct',recl=size(ttnd)*8)
+    ! read  (91,rec=1) T
+    ! close (91)
+    ! open  (92,file='./INPUT/q.dat',access='direct',recl=size(ttnd)*8)
+    ! read  (92,rec=1) q
+    ! close (92)
+    ! open  (92,file='./INPUT/w.dat',access='direct',recl=size(ttnd)*8)
+    ! read  (92,rec=1) w
+    ! close (92)
+    
+    print*, nz, ztop
+
+    ! dz 계산
+    ! dz = compute_dz(ztop, nz, grid_dz)    ! [m]
+
+    ! time integration
+    ! do it = 1, nt
+    !     call compute_advection(w, T)
+    ! end do
+
+    ! output result
+
 end program driver
