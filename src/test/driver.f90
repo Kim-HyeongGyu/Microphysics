6f491acb6ae62ea8244b1469d79627b173ed26f1
Kim-HyeongGyu <hyeonggyu.kim01@gmail.com>
2020-03-30 15:11:54 +0900

    boundary == 0

--- src/test/driver.f90
+++ src/test/driver.f90
@@ -1,21 +1,19 @@
 program driver
 use          constant_mod, only: show_constant
 use   vert_coordinate_mod, only: compute_vert_coord
-! use         advection_mod, only: compute_advection
+use         advection_mod, only: compute_advection
 implicit none
 
     ! Set variables
-    integer :: nz, it
+    integer :: nz, it, i 
     integer, parameter :: nt = 100  ! TODO! nt = t_final/dt
-    real    :: ztop, zbottom
+    real    :: ztop, zbottom = 0.   ! default bottom [km]
     real    :: GRAVITY, PI
-    real, dimension(:), allocatable :: dz
+    real, dimension(:), allocatable :: z_full, z_half
     real, dimension(:), allocatable :: T, q, w
     ! real, dimension(n+1) :: array_foreward, array_backward
     character(len=20) :: grid_dz, diff_method
-    namelist /setup_list/ nz, ztop, zbottom, grid_dz, diff_method
-
-    zbottom = 0.    ! default bottom [km]
+    namelist /setup_list/ nz, ztop, grid_dz, diff_method
 
     open  (unit = 8, file = 'input.nml', delim = 'apostrophe')
     read  (unit = 8, nml  = setup_list) 
@@ -32,27 +30,28 @@ implicit none
     ! read  (92,rec=1) w
     ! close (92)
 
+    T = (/ (I+273, I = 20,1,-1) /)  ! lapse rate 1K/km
+    w = (/ (I, I = 1,40,2) /)
+
     print*, "========= Setup variables ========="
     print*, "Num of z-grid   : ", nz
     print*, "Top of model    : ", ztop
-    print*, "Bottom of model : ", zbottom
     print*, "Grid type       : ", grid_dz
     print*, "Integration method : ", diff_method
     print*, "==================================="
     ! call show_constant()
 
     ! Calculate dz
-    allocate(dz(nz))
-    call compute_vert_coord(ztop, zbottom, nz, grid_dz, dz)    ! [m]
+    allocate(z_full(nz), z_half(nz+1))
+    call compute_vert_coord(ztop, zbottom, nz, grid_dz, z_full, z_half)
 
     ! time integration
-    ! do it = 1, nt
-    !     call compute_advection(w(it,:), T(it,:), diff_method)
-    ! end do
+    ! call compute_advection(w, T, nt, diff_method)
 
     ! output result
 
     ! deallocate storage
-    if (allocated(dz)) deallocate(dz)
+    if (allocated(z_full)) deallocate(z_full)
+    if (allocated(z_half)) deallocate(z_half)
 
 end program driver
