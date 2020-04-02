1288e03d839d8cf3290ee698f19b28cccf5b8f04
JiHyun-Yun <jhyun.neuru@gmail.com>
2020-04-02 13:26:51 +0900

    update 'stretching_gird' in vert_coordinate.f90

--- src/test/vert_coordinate.f90
+++ src/test/vert_coordinate.f90
@@ -1,31 +1,34 @@
 module vert_coordinate_mod
 contains
-    subroutine compute_vert_coord(ztop, zbottom, nz, grid_dz, z_full, z_half)
+    subroutine compute_vert_coord(ztop, nz, grid_dz, z_full, z_half)
     implicit none
-    integer, intent(in) :: nz                ! Num of z_full
-    real,    intent(in) :: ztop, zbottom     ! [km]
+    real,    intent(in) :: ztop     ! [km]
+    integer, intent(in) :: nz       ! Num of z_full
     character(len=*), intent(in)       :: grid_dz
     real, intent(out), dimension(nz)   :: z_full
     real, intent(out), dimension(nz+1) :: z_half
     integer :: k
-    real, dimension(nz) :: dz   ! delta z for z_half
+    real    :: dz       ! delta z for z_half
+    real    :: zbottom  ! [km]
 
     select case (grid_dz)
         case ("constant_grid")
+            zbottom = 0.
             dz = (ztop - zbottom) * 1000. / nz
             z_half(1) = zbottom * 1000.
             do k = 1, nz
-                z_half(k+1) = z_half(k) + dz(k)
+                z_half(k+1) = z_half(k) + dz
             end do
             z_full = z_half(1:nz) + dz/2.
         case ("stretching_grid")
-            print*, "TODO! need to work..."
-            stop
-            ! do k = 1, nz
-            !     dz(k) = dz(k-1)*dzr
-            !     print*, dz(k)
-            ! end do
-            ! calc_ztop = dz(1)*(dzr-1)/(dzr-1)
+            zbottom = 1E-3
+            dz = (ztop*1000.)**(1.0/REAL(nz))
+            z_half(1) = zbottom*1000.
+            do k = 1, nz
+                z_half(k+1) = z_half(k) * dz
+                z_full(k)   = z_half(k) + ((z_half(k+1)-z_half(k))/2.)
+            end do
+            calc_ztop = dz(1)*(1-dzr**nz)/(1-dzr)
         case default
             print*, "Not setup grid_dz option. &
                      please check input.nml"
