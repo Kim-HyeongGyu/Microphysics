e70f9e7d423ece19b3a9aa6bbbf298d2beea0c38
JiHyun-Yun <jhyun.neuru@gmail.com>
2020-04-14 01:44:40 +0900

    Update variable transformation codes (...ING)

--- src/Yun/var_trans.f90
+++ src/Yun/var_trans.f90
@@ -2,32 +2,57 @@
 
     implicit none
 
-    integer :: nz=20, nlev=37
-!    integer, intent(in) :: nz
-!    integer, intent(in) :: nlev
-    real, dimension(nlev) :: qv_in=0.02 ! [kg/kg], specific humidity
-    real, dimension(nlev) :: Tin        ! [K]
-    real, dimension(nlev) :: Pin        ! 
-!    real, dimension(nlev), intent(in) :: qv_in ! [kg/kg], specific humidity
-!    real, dimension(nlev), intent(in) :: Tin   ! [K]
-!    real, dimension(nlev), intent(in) :: Pin   !
-    real, dimension(nz) :: qv_out, Tout, Pout
-!    real, dimension(nz), intent(out) :: qv_out, Tout, Pout
-    real, dimension(nlev) :: Tv     ! [K], virtual Temperature
-    real, parameter :: R  = 287.    ! [J/(kg K)]
-    real, parameter :: g  = 9.8     ! [m/s^2]
-    real :: H       ! [m] sclae height
-!    character(len=*) :: vert_var, temp_var
+!    character(len=*), intent(in) :: vert_var, temp_var     !! NAMELIST...??
+    integer :: nz=20, nlev=40, I
+!    integer, intent(in) :: nz, nlev
+    real, dimension(nlev) :: qv_in      ! [kg/kg], specific humidity
+    real, dimension(nlev) :: temp_in    ! [K]
+    real, dimension(nlev) :: vert_in
+!    real, dimension(nlev), intent(in) :: qv_in, temp_in, vert_in
+    real, parameter :: Ps = 1013.  ! [hPa] surface pressure
+!    real, parameter, intent(in) :: Ps  ! [hPa] surface pressure
+    real, dimension(nz) :: z_out, Tout, qv_out
+!    real, dimension(nz), intent(out) :: z_out, Tout, qv_out
+    real, dimension(nlev) :: z_in, T_in
+    real, dimension(nlev) :: Tv     ! [K], virtual temperature
+    real, dimension(nlev) :: H      ! [m] sclae height
+    real, parameter :: g  = 9.8     ! [m s-2]
+    real, parameter :: R  = 287.    ! [J kg-1 K-1]
+    real, parameter :: Cp = 1003.5  ! [J kg-1 K-1] specific heat at constant pressure
 
-    qv = 20.
-    Tv = Tin*(1+(0.61*qv_in*0.001))
-    H = (R*Ts)/g
+!!!!! :: SETTING VARIABLE
 
-!    if ( vert_var == p ) then
-        
+    qv_in = 0.02
+    temp_in = 298.
+    vert_in(1) = 1000. 
+    do i = 1, nlev-1
+        vert_in(i+1) = vert_in(i) - 25.
+    enddo 
+
+!!!!! :: (VARIABLES) to (z, T, qv)
+
+!    if ( vert_var == 'p' ) then
+        Tv = temp_in*(1+(0.61*qv_in))
+        H = (R*Tv)/g
+        z_in = -H*(log(vert_in/Ps)) 
+        print*, z_in
+!    else
+!       z_in = vert_in
 !    endif
-!    if ( temp_var == theta ) then
 
+!    if ( temp_var == 'theta' ) then
+!        if ( vert_var == 'p' ) then 
+!            T_in = temp_in*((vert_in/Ps)**(R/Cp))
+!        else
+!            print*, " :: Without air pressure information,"
+!            print*, " ::  'Temp' can't be calculated from 'theta'."
+!            print*, " :: Please Check the 'vertical variable'."
+!        endif
+!    else
+       T_in = temp_in    
 !    endif
+
+!!!!! :: Interpolate to fit nz
+
 
     endprogram
