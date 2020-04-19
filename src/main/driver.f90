4b35a5de30aa2b085e8b8199d4d4cf69e8df8a5b
Kim-HyeongGyu <hyeonggyu.kim01@gmail.com>
2020-04-20 00:32:03 +0900

    Solve problem

--- src/main/driver.f90
+++ src/main/driver.f90
@@ -15,24 +15,29 @@ implicit none
 
     call read_namelist()
 
-    call read_data_init(nlev,lev,temp_in,qv_in,w_in)
+    call read_data_init(nlev, lev, temp_in, qv_in, w_in)
 
 
     ! Calculate dz
-    call compute_vert_coord(ztop, zbottom, nz,vertical_grid, &
+    call compute_vert_coord(ztop, zbottom, nz, vertical_grid, &
                             z_full, z_half, dz)
     call make_bins()
     call conc_dist()
 
+    ! interplate 1d
+    call interpolate_1d(vert_var, temp_var, z_full, qv_in,    &
+                         temp_in, w_in, lev, Tinit, qinit, winit)
 
     ! Comupte dt using CFL conditin
-    call compute_dt_from_CFL(CFL_condition, dz, w_in, nt, dt)
-    ! interplate 1d
-    call interpolate_1d(vert_var,temp_var,z_full,qv_in,temp_in,w_in,lev,Tinit,qinit,winit)
+    call compute_dt_from_CFL(CFL_condition, dz, winit, nt, dt)
     allocate(T(nz,nt), q(nz,nt), w(nz) )
     T(:,1) = Tinit
     q(:,1) = qinit
     w = winit
+    q      = 0
+    q(5,1) = 100.
+    w = 1.
+    print*, q(:,1)
  
     call show_setup_variables()    
 
@@ -45,6 +50,9 @@ implicit none
                                vertical_advect, q(:,n+1))
     end do
 
+    do i = 1, nt
+        print*, sum (q(:,i))
+    end do
 
     call write_data()
  
