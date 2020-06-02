db5dc3e485d5cfa7ed7d754b5544d88712460d45
Kim-HyeongGyu <hyeonggyu.kim01@gmail.com>
2020-06-02 19:57:54 +0900

    Add substeping

--- src/gyu/substeping.f90
+++ src/gyu/substeping.f90
@@ -1,7 +1,7 @@
 module substeping_mod
 
 interface time_substeping
-    module procedure time_substeping_1d, time_substeping_2d
+    module procedure time_substeping_1d!, time_substeping_2d
 end interface
 
 contains
@@ -14,9 +14,16 @@ contains
 
         real :: courant_number
 
-        ! Trick: size(dz) /= size(W), So we use max value of W for calculate
-        ! curant number. Strictly, W must be interpolated as z_full coordinate.
-        courant_number = maxval( dt/dz * maxval(abs(W)) )
+        if ( size(dz) /= size(W) ) then
+            ! Dynamics substeping
+            ! Note: size(dz) /= size(W), So we use max value of W for calculate
+            ! curant number. Strictly, W must be interpolated as z_full coordinate.
+            courant_number = maxval( dt/dz * maxval(abs(W)) )
+        else
+            ! Physics substeping
+            ! courant_number = maxval( dt/dm * abs(dm_dt) )
+            courant_number = maxval( dt/dz * abs(W) )
+        end if
 
         if (courant_number > 1) then 
             dt = dt / 2.
@@ -26,29 +33,4 @@ contains
         
     end subroutine time_substeping_1d
 
-    !TODO: check input argument
-    recursive subroutine time_substeping_2d( dmb_dt, mb, dt, num_substep )
-        implicit none
-        real, dimension(:,:), intent(in)  :: dmb_dt
-        real, dimension(:,:), intent(in)  :: mb
-        real,               intent(inout) :: dt
-        integer,            intent(inout) :: num_substep
-
-        real :: courant_number
-        real, dimension(100) :: dm
-        
-        dm = mb(2:,1) - mb(1:size(mb)-1,1)
-
-        ! Trick: size(dz) /= size(W), So we use max value of W for calculate
-        ! curant number. Strictly, W must be interpolated as z_full coordinate.
-        courant_number = maxval( dt/dm * maxval(abs(dmb_dt)) )
-
-        if (courant_number > 1) then 
-            dt = dt / 2.
-            num_substep = num_substep * 2
-            call time_substeping_2d( dmb_dt, mb, dt, num_substep )
-        end if
-
-    end subroutine time_substeping_2d
-    
 end module substeping_mod
